633e3c460b4786fe89d8cee711b10c5ed9cee934
Michael Bombini <bombini.michael@gmail.com>
2022-09-07 23:27:41 +0200

    Started working on nav and darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <nav>
-      <!-- <router-link to="/">Home</router-link> | -->
-      <!-- <h1 class="dark:text-3xl font-bold underline bg-red-800 text-center text-white">Hello world!</h1> -->
-      <p>Lorem, ipsum dolor.</p>
-    </nav>
+    <TheNav/>
+    <!-- <nav>
+     <router-link to="/">Home</router-link> | 
+    <h1 class="dark:text-3xl font-bold underline bg-red-800 text-center text-white">Hello world!</h1> 
+    </nav> -->
+    
     <router-view />
   </div>
 </template>
 
 <script>
+import TheNav from './components/TheNav.vue';
 export default{
-  
+    components: { TheNav }
 }
 
 </script>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="container mx-auto justify-between flex p-3 items-center">
+        <div>
+            Where in the world?
+        </div>
+        <div>
+            Dark Mode
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
