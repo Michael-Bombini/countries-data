94a1d5e4bdad88ec5669bff5ac1011e3b7bb2ac1
Michael Bombini <bombini.michael@gmail.com>
2022-09-08 22:56:55 +0200

    Added dark mode first check

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,20 +1,40 @@
 <template>
-    <nav class="container mx-auto justify-between flex p-3 items-center">
-        <div>
-            Where in the world?
-        </div>
-        <div>
-            Dark Mode
-        </div>
+  <header class="bg-primary">
+    <nav class="container mx-auto justify-between p-6 flex items-center">
+      <h1>Where in the world?</h1>
+      <img :src="require('')" alt="">
+      <button class="" @click="darkMode()">
+        clickclick
+      </button>
     </nav>
+    
+  </header>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  name: "TheNav",
+  data() {
+    return {};
+  },
+  methods: {
+    darkMode() {
+      const html = document.querySelector("html");
+      html.classList.add("dark");
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+
+.bg-primary {
+  @apply bg-white dark:bg-orange-400;
+}
+
+h1 {
+    font-size: 1.2rem;
+    font-weight: bold;
+}
 
 </style>
