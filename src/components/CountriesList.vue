54c2884bbfb6334f20186b8b6ee571b55e0dda76
Michael Bombini <bombini.michael@gmail.com>
2022-09-12 00:12:40 +0200

    Completed multiple filter country name and region

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,13 +1,29 @@
 <template>
   <section class="container mx-auto">
-    <input type="text" name="" id="" v-model="countryName" class="border-red-500 border" placeholder="Search country">
+    <input
+      type="text"
+      name=""
+      id=""
+      v-model="countryName"
+      class="border-red-500 border"
+      placeholder="Search country"
+    />
+    <select name="region" id="region" v-model="countryRegion">
+      <option value="">Filter by Region</option>
+      <option value="africa">Africa</option>
+      <option value="americas">America</option>
+      <option value="asia">Asia</option>
+      <option value="europe">Europe</option>
+      <option value="oceania">Oceania</option>
+    </select>
+ 
+
     <div class="container mx-auto py-12 text">
       <div class="grid grid-cols-4 gap-12">
         <div v-for="(country, i) in countriesList" :key="i" class="">
           <TheCountry :country="country" />
         </div>
       </div>
-
     </div>
   </section>
 </template>
@@ -21,7 +37,8 @@ export default {
   data() {
     return {
       countries: [],
-      countryName : "",
+      countryName: "",
+      countryRegion: "",
     };
   },
   methods: {
@@ -30,18 +47,34 @@ export default {
         this.countries = resp.data;
       });
     },
-  
   },
-  computed : {
-    countriesList(){
-      if(this.countryName.trim().length === 0)
-        return this.countries;
-      else 
-      return this.countries.filter((country) => country.name.common.toLowerCase().includes(this.countryName.toLowerCase()));
+  computed: {
+    countriesList() {
+      if (this.countryName.trim().length === 0 && this.countryRegion === "" ) return this.countries;
+      else
+        return this.countries.filter((country) =>
+          country.name.common
+            .toLowerCase()
+            .includes(this.countryName.toLowerCase())
+          &&
+          country.region.toLowerCase().includes(this.countryRegion)
+        );
+
+
         
+    },
+
+    // checkRegion() {
+    //   if (!this.countryRegion) return this.countries;
+    //   else
+    //     return this.countries.filter((country) =>
+    //       country.region
+    //         .toLowerCase()
+    //         .includes(this.countryRegion)
+    //     );
+    // }
 
 
-    }
   },
   mounted() {
     this.fetchData();
